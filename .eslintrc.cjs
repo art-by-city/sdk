ee5291ca874a2ef9d5fc17c518ec8aa79d1b5fc1
Jim Toth <jim@artby.city>
2023-08-24 20:02:59 -0400

    Adds linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  // env: {
+  //   browser: true
+  // },
+  // overrides: [{
+  //   files: [ 'src/*.ts' ]
+  // }],
+  ignorePatterns: ['dist/*'],
+  plugins: [ '@typescript-eslint' ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json'
+    // project: true,
+    // tsconfigRootDir: __dirname
+  },
+  extends: [
+    'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended-type-checked'
+  ],
+  rules: {
+    indent: [ 'error', 2 ],
+    'max-len': [ 'error', {
+      code: 80,
+      tabWidth: 2
+    }],
+  }
+}
