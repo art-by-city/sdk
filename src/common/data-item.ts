ec96b7c4b9f42c1ef488f7eb6534fc86780e7114
Jim Toth <jim@artby.city>
2024-02-23 15:58:54 -0500

    Manually set data item rawId when signing in browser

--- src/common/data-item.ts
+++ src/common/data-item.ts
@@ -12,8 +12,14 @@ export default class DataItemFactory {
   ): Promise<DataItem> {
     if (this.signer instanceof InjectedArweaveSigner) {
       const signed = await window.arweaveWallet.signDataItem({ data, tags })
+      const dataItem = new DataItem(Buffer.from(signed))
+      
+      // NB: ArConnect requires the above method of signing DataItem, but they
+      //     fail arbundle's .isSigned() check when re-instantiated because
+      //     ._id isn't set.  So we set it by assigning .rawId to itself.
+      dataItem.rawId = dataItem.rawId
 
-      return new DataItem(Buffer.from(signed))
+      return dataItem
     }
     
     const dataItem = createData(data, this.signer, { tags })
